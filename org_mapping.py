eddb2f749f649ab30bdab754823811520483b85d
Tim Fairweather <tim.fairweather@arctiq.ca>
2017-06-13 11:52:21 -0400

    Added CN instead of OU

--- org_mapping.py
+++ org_mapping.py
@@ -1,21 +1,21 @@
 {
  "Dept1": {
   "users": [
-   "CN=Dept1_Users,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept1_Users,CN=Users,DC=cloud,DC=lab"
   ],
   "remove_users": true,
   "admins": [
-   "CN=Dept1_Admins,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept1_Admins,CN=Users,DC=cloud,DC=lab"
   ],
   "remove_admins": true
  },
  "Dept2": {
   "users": [
-   "CN=Dept2_Users,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept2_Users,CN=Users,DC=cloud,DC=lab"
   ],
   "remove_users": true,
   "admins": [
-   "CN=Dept2_Admins,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept2_Admins,CN=Users,DC=cloud,DC=lab"
   ],
   "remove_admins": true
  }
