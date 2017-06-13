111e7a7467b7b12070e3c4ee2884b7e96a7c15f8
Tim Fairweather <tim.fairweather@arctiq.ca>
2017-06-13 11:35:02 -0400

    Change quotes to proper double quote

---
+++ org_mapping.py
@@ -0,0 +1,22 @@
+{
+ "Dept1": {
+  "users": [
+   "Dept1_Users,OU=Users,DC=cloud,DC=lab"
+  ],
+  "remove_users": true,
+  "admins": [
+   "Dept1_Admins,OU=Users,DC=cloud,DC=lab"
+  ],
+  "remove_admins": true
+ },
+ "Dept2": {
+  "users": [
+   "Dept2_Users,OU=Users,DC=cloud,DC=lab"
+  ],
+  "remove_users": true,
+  "admins": [
+   "Dept2_Admins,OU=Users,DC=cloud,DC=lab"
+  ],
+  "remove_admins": true
+ }
+}
