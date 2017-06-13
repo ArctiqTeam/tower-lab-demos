a88882f878dbb28183e76f38ed4942723cc649c2
Tim Fairweather <tim.fairweather@arctiq.ca>
2017-06-13 11:39:41 -0400

    Added CN

--- org_mapping.py
+++ org_mapping.py
@@ -1,21 +1,21 @@
 {
  "Dept1": {
   "users": [
-   "Dept1_Users,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept1_Users,OU=Users,DC=cloud,DC=lab"
   ],
   "remove_users": true,
   "admins": [
-   "Dept1_Admins,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept1_Admins,OU=Users,DC=cloud,DC=lab"
   ],
   "remove_admins": true
  },
  "Dept2": {
   "users": [
-   "Dept2_Users,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept2_Users,OU=Users,DC=cloud,DC=lab"
   ],
   "remove_users": true,
   "admins": [
-   "Dept2_Admins,OU=Users,DC=cloud,DC=lab"
+   "CN=Dept2_Admins,OU=Users,DC=cloud,DC=lab"
   ],
   "remove_admins": true
  }
--- team_mapping.py
+++ team_mapping.py
@@ -1,12 +1,12 @@
 {
  "Dept1 Users": {
   "organization": "Dept1",
-  "users": "Dept1_Users,OU=Users,DC=cloud,DC=lab",
+  "users": "CN=Dept1_Users,OU=Users,DC=cloud,DC=lab",
   "remove": true
   },
  "Dept2 Users": {
   "organization": "Dept2",
-  "users": "Dept2_Users,OU=Users,DC=cloud,DC=lab",
+  "users": "CN=Dept2_Users,OU=Users,DC=cloud,DC=lab",
   "remove": true
   }
 }
